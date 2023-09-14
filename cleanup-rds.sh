95baf2e1023880e4a7ac49e103fc3ef62d15966d
itlabrepo <arthur.luox@gmail.com>
2023-09-14 18:57:41 +1000

    Update cleanup-rds.sh

--- cleanup-rds.sh
+++ cleanup-rds.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
 
-# Initialize an empty string to store the names of RDS databases that couldn't be deleted
-UNDELETED_RDS=""
+# Initialize empty strings to store resources that cannot be deleted
+UNDELETABLE_INSTANCES=""
+UNDELETABLE_CLUSTERS=""
+UNDELETABLE_SNAPSHOTS=""
 
 # Get a list of all AWS regions
 REGIONS=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 
+# For each region, list and attempt to delete RDS DB instances, clusters, and snapshots
 for REGION in $REGIONS; do
     echo "Checking region: $REGION"
 
-    # List all RDS databases in the current region
-    RDS_DBS=$(aws rds describe-db-instances --region $REGION --query "DBInstances[].DBInstanceIdentifier" --output text)
-
-    for DB in $RDS_DBS; do
-        echo "Attempting to delete RDS database: $DB in region: $REGION"
-
-        # Try to delete the RDS database (with the --skip-final-snapshot option to avoid creating a final snapshot)
-        if aws rds delete-db-instance --db-instance-identifier $DB --skip-final-snapshot --region $REGION ; then
-            echo "Successfully deleted RDS database: $DB in region: $REGION"
+    # Delete RDS DB instances
+    INSTANCES=$(aws rds describe-db-instances --region $REGION --query "DBInstances[].DBInstanceIdentifier" --output text)
+    for INSTANCE_ID in $INSTANCES; do
+        # First, modify the instance to disable deletion protection
+        aws rds modify-db-instance --db-instance-identifier $INSTANCE_ID --no-deletion-protection --region $REGION
+        if aws rds delete-db-instance --db-instance-identifier $INSTANCE_ID --region $REGION --skip-final-snapshot ; then
+            echo "Successfully deleted RDS DB instance: $INSTANCE_ID in region: $REGION"
         else
-            echo "Failed to delete RDS database: $DB in region: $REGION"
-            UNDELETED_RDS="$UNDELETED_RDS $DB:$REGION"
+            UNDELETABLE_INSTANCES="$UNDELETABLE_INSTANCES $INSTANCE_ID:$REGION"
+        fi
+    done
+
+    # Delete RDS DB clusters
+    CLUSTERS=$(aws rds describe-db-clusters --region $REGION --query "DBClusters[].DBClusterIdentifier" --output text)
+    for CLUSTER_ID in $CLUSTERS; do
+        # First, disable deletion protection
+        aws rds modify-db-cluster --db-cluster-identifier $CLUSTER_ID --no-deletion-protection --region $REGION
+        if aws rds delete-db-cluster --db-cluster-identifier $CLUSTER_ID --region $REGION --skip-final-snapshot ; then
+            echo "Successfully deleted RDS DB cluster: $CLUSTER_ID in region: $REGION"
+        else
+            UNDELETABLE_CLUSTERS="$UNDELETABLE_CLUSTERS $CLUSTER_ID:$REGION"
+        fi
+    done
+
+    # Delete RDS snapshots
+    SNAPSHOTS=$(aws rds describe-db-snapshots --region $REGION --query "DBSnapshots[].DBSnapshotIdentifier" --output text)
+    for SNAPSHOT_ID in $SNAPSHOTS; do
+        if aws rds delete-db-snapshot --db-snapshot-identifier $SNAPSHOT_ID --region $REGION ; then
+            echo "Successfully deleted RDS snapshot: $SNAPSHOT_ID in region: $REGION"
+        else
+            UNDELETABLE_SNAPSHOTS="$UNDELETABLE_SNAPSHOTS $SNAPSHOT_ID:$REGION"
         fi
     done
 done
 
-# Check if there were any RDS databases that couldn't be deleted
-if [ -n "$UNDELETED_RDS" ]; then
-    echo "The following RDS databases could not be deleted:"
-    for DB in $UNDELETED_RDS; do
-        echo $DB
+# Print resources that could not be deleted
+if [ ! -z "$UNDELETABLE_INSTANCES" ]; then
+    echo "The following RDS DB instances could not be deleted:"
+    for INSTANCE in $UNDELETABLE_INSTANCES; do
+        echo $INSTANCE
     done
-else
-    echo "All RDS databases deleted successfully."
+fi
+
+if [ ! -z "$UNDELETABLE_CLUSTERS" ]; then
+    echo "The following RDS DB clusters could not be deleted:"
+    for CLUSTER in $UNDELETABLE_CLUSTERS; do
+        echo $CLUSTER
+    done
+fi
+
+if [ ! -z "$UNDELETABLE_SNAPSHOTS" ]; then
+    echo "The following RDS snapshots could not be deleted:"
+    for SNAPSHOT in $UNDELETABLE_SNAPSHOTS; do
+        echo $SNAPSHOT
+    done
 fi
